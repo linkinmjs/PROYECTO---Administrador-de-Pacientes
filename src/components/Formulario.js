6a143501765c8232ae5e8ec6aedff5ba7f4cb9dc
Mauri <linkinmjs@gmail.com>
2020-02-26 12:28:47 -0300

    Creando el state para las citas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,22 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
 const Formulario = () => {
+
+    // crear state de Citas
+
+    const[cita,actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha:'',
+        hora:'',
+        sintomas:''
+    })
+
+    // función que se ejecuta cada vez que el usuario escribe en el input
+    const actualizarState= () => {
+        console.log('escribiendo...');
+    }
+
     return(
         <Fragment>
             <h2>Crear Cita</h2>
@@ -12,6 +28,7 @@ const Formulario = () => {
                     name="Mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={actualizarState} //o handleChange
                 />
                 <label>Dueño de la Mascota</label>
                 <input 
@@ -19,23 +36,27 @@ const Formulario = () => {
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del dueño de la Mascota"
+                    onChange={actualizarState} //o handleChange
                 />
                 <label>fecha</label>
                 <input 
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState} //o handleChange
                 />
                 <label>Hora</label>
                 <input 
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState} //o handleChange
                 />
                 <label>Síntomas</label>
                 <textarea
-                    name="hora"
+                    name="sintomas"
                     className="u-full-width"
+                    onChange={actualizarState} //o handleChange
                 ></textarea>
 
                 <button
